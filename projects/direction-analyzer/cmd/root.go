5c6137dec2b6fe1f88395231b73e97acba1fd875
Konstantin Kim <k.kim@mixvel.com>
2026-07-10 17:17:17 +0300

    wip

--- projects/direction-analyzer/cmd/root.go
+++ projects/direction-analyzer/cmd/root.go
@@ -4,19 +4,57 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
+
+var (
+	rootPath string
+)
+
+type FileInfo struct {
+	Folder, Name string
+	Size         int64
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "direct-analyzer",
 	Short: "Утилита для подсчёта статистики в директории (количество файлов, размер, строки).",
-	Long:  `Утилита для подсчёта статистики в директории (количество файлов, размер, строки).`,
+	Long:  "Утилита для подсчёта статистики в директории (количество файлов, размер, строки).",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		infos := []FileInfo{}
+		rootPathLen := len(rootPath)
+		filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			info, err := d.Info()
+			if err != nil {
+				return err
+			}
+			if !d.IsDir() {
+				fileName := info.Name()
+				folder := path[rootPathLen:]
+				folder = folder[:len(folder)-len(fileName)]
+				infos = append(infos, FileInfo{folder, fileName, info.Size()})
+			}
+
+			return nil
+		})
+		writer := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		for _, info := range infos {
+			fmt.Fprintf(writer, "%s\t%s\t%d\n", info.Folder, info.Name, info.Size)
+		}
+		writer.Flush()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,5 +75,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().StringP("path", "p", ".", "Direction to analyze")
+	rootCmd.Flags().StringVar(&rootPath, "path", ".", "Direction to analyze")
 }
